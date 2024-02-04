b0ee2d07fa3efb48d8f4de22cb556c5d102e85f1
zhulongkang <zhulongkang@qince.com>
2024-02-04 17:58:39 +0800

    系统监测优化

--- seed-common/src/main/java/com/seed/mapper/system/SysMoitorMapper.java
+++ seed-common/src/main/java/com/seed/mapper/system/SysMoitorMapper.java
@@ -13,18 +13,10 @@ import java.util.Set;
 */
 public interface SysMoitorMapper {
 
-    int deleteByPrimaryKey(Long id);
-
     int insert(SysMoitor record);
 
     void insertBatch(@Param("sysMoitorSet") Set<SysMoitor> sysMoitorSet);
 
     int insertSelective(SysMoitor record);
-
-    SysMoitor selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(SysMoitor record);
-
-    int updateByPrimaryKey(SysMoitor record);
 
 }
